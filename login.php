80738c86e4dbe86dd83fce840225d0c717dc98c2
Kishiannn_ <abudachan122@gmail.com>
2025-02-07 23:24:25 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,29 @@
+<?php
+session_start();
+include "db_conn.php";
 
+if(isset($_POST["username"])) && isset($_POST["password"])) {
+
+    function validate($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return data;
+    }
+}
+
+$username = validate($_POST["username"]);
+$password = validate($_POST["password"]);
+
+if(empty($username)){
+    header("Location: index.php?error=USERNAME is required")
+    exit();
+}
+else if(empty($password)){
+    header("Location: index.php?error=PASSWORD is required")
+    exit();
+}
+
+
+
+?>
