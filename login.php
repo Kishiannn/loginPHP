984a382b84b8f74cc343e93b020b2c9a9b1d4868
Kishiannn_ <abudachan122@gmail.com>
2025-02-07 23:33:11 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 include "db_conn.php";
 
-if(isset($_POST["username"])) && isset($_POST["password"])) {
+if(isset($_POST["username"]) && isset($_POST["password"])) {
 
     function validate($data){
         $data = trim($data);
@@ -15,15 +15,29 @@ if(isset($_POST["username"])) && isset($_POST["password"])) {
 $username = validate($_POST["username"]);
 $password = validate($_POST["password"]);
 
-if(empty($username)){
-    header("Location: index.php?error=USERNAME is required")
+if(empty($username)) {
+    header ("Location: index.php?error=USERNAME is required");
     exit();
 }
 else if(empty($password)){
-    header("Location: index.php?error=PASSWORD is required")
+    header("Location: index.php?error=PASSWORD is required");
     exit();
 }
 
+$sql = "SELECT * FROM user WHERE user_name='username' AND password ='password'";
 
+$result = mysqli_query($conn, $sql);
 
+if(mysqli_num_rows($result) === 1){
+    $row = mysqli_fetch_assoc($result);
+    if($row["user_name"] == $username && $row["password" == $password]) {
+        echo "Logged In";
+        $_SESSION['user_name'] = $row['username'];
+        $_SESSION['name'] = $row['name'];
+        $_SESSION['id'] = $row['id'];
+        header("Location: home.php");
+        exit();
+    }
+    
+}
 ?>
